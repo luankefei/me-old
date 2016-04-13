954c2d8005c8618678a70810141570dd616df31d
luankefei <luankefei@gmail.com>
2016-04-13 20:32:36 +0800

    格式调整

--- api/controllers/routes.js
+++ api/controllers/routes.js
@@ -6,8 +6,8 @@ import auth from './auth';
  * 设置路由
  * @param  {koa-router} router
  */
- export default router => {
-    router.get('/auth', auth.loadAuth);
-    router.post('/auth', auth.login);
-    router.del('/auth', auth.logout);
- };
+export default router => {
+  router.get('/auth', auth.loadAuth);
+  router.post('/auth', auth.login);
+  router.del('/auth', auth.logout);
+};
