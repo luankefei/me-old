dbe710364afd9db05e880019956bf8f494fff242
luankefei <luankefei@gmail.com>
2016-04-20 21:06:48 +0800

    补全部分api

--- api/controllers/routes.js
+++ api/controllers/routes.js
@@ -10,4 +10,12 @@ export default router => {
   router.get('/auth', auth.loadAuth);
   router.post('/auth', auth.login);
   router.del('/auth', auth.logout);
+
+  // router.get('/content', content.getContent)
+  // router.post('/content', content.addContent)
+  // router.get('/content/:id', content.getContentById)
+  // router.delete('/content', content.deleteContentById)
+  //
+  // router.post('/comment', comment.addComment)
+  // router.get('/comment/:id', comment.getCommentById)
 };
