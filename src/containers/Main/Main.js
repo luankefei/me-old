97b77e35a37def1ccb6fad69a7a96a16bd88ec68
luankefei <luankefei@gmail.com>
2016-07-01 18:04:25 +0800

    main不再是pure component

--- src/containers/Main/Main.js
+++ src/containers/Main/Main.js
@@ -1,21 +1,29 @@
-import React, {PropTypes} from 'react';
+import React, { Component, PropTypes } from 'react';
 // import React from 'react';
 import {Nav, Foot} from 'components';
 
-export default function Main(props) {
-  const styles = require('./Main.scss');
-  return (
-    <div className={styles.main}>
-      <Nav />
-      <div>{props.children}</div>
-      <Foot />
-    </div>
-  );
+export default class Main extends Component {
+  static propTypes = {
+    children: PropTypes.object.isRequired
+  };
+
+  constructor(props) {
+    super(props);
+  }
+
+  state = {
+    test: 0
+  };
+
+  render() {
+    const styles = require('./Main.scss');
+    const {children} = this.props;
+    return (
+      <div className={styles.main}>
+        <Nav />
+        <div>{children}</div>
+        <Foot />
+      </div>
+    );
+  }
 }
-
-
-// <div ref='view' className={styles.container}>{this.props.children}</div>
-
-Main.propTypes = {
-  children: PropTypes.object.isRequired
-};
