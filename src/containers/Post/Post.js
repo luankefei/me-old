07ad1c1187f5c2df4eb7585cc5cb1080b558dcda
luankefei <luankefei@gmail.com>
2016-07-04 20:03:49 +0800

    引入ckeditor

--- src/containers/Post/Post.js
+++ src/containers/Post/Post.js
@@ -11,61 +11,74 @@ export default class Post extends Component {
     login: PropTypes.func
   }
 
+  constructor(props) {
+    super(props);
+
+    this.editorLoaded = false;
+  }
+
+  componentDidMount() {
+    if (!this.editorLoaded) {
+      this.initEditor();
+    }
+  }
+
+  initEditor() {
+    const script = document.createElement('script');
+    script.src = '/ckeditor/ckeditor.js';
+    script.onload = () => {
+      window.CKEDITOR.replace(this.refs.editor, {
+        height: 500
+      });
+    };
+    document.body.appendChild(script);
+    console.log(script);
+  }
+
   render() {
     const styles = require('./Post.scss');
     return (
       <div className={styles.post}>
-        <div ref='editor'>
-          <textarea name='ckeditor' id='' cols='30' rows='10'></textarea>
+        <div className={styles.editor}>
+          <textarea ref='editor' name='ckeditor' id='' cols='30' rows='10'></textarea>
         </div>
-        <div ref='form'>
+        <div ref='form' className={styles.form}>
           <dl className={styles.clearfix}>
             <dt>文章类型</dt>
             <dd>
-              <div ref='type' className={styles.select} data-id='3'>
-                <span className={styles['select-value']}></span>
-                <i></i>
-                <div className={styles['select-wrapper']}>
-                  <div className={styles['select-list']}>
-                    <ul>
-                      <li>评测</li>
-                      <li>推荐</li>
-                      <li>新闻</li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
+              <input type='text' />
             </dd>
           </dl>
 
           <dl className={styles.clearfix}>
             <dt>作者</dt>
             <dd>
-            <div ref='author' className={styles.select} data-id='1'>
-              <span className={styles['select-value']}>sunken</span>
-              <i></i>
-              <div className={styles['select-wrapper']}>
-                <div className={styles['select-list']}>
-                  <ul>
-                    <li>sunken</li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-            <label className={styles.timer}>定时发送</label>
-            <input className={styles.textbox} type='text' placeholder='2014.11.11 11:11:11' />
+              <input type='text' placeholder='sunken' />
+            </dd>
+          </dl>
+          <dl>
+            <dt>定时发送</dt>
+            <dd>
+              <input className={styles.textbox} type='text' placeholder='2014.11.11 11:11:11' />
             </dd>
           </dl>
           <dl className={styles.clearfix}>
             <dt>标题</dt>
             <dd>
-            <input ref='title' className='textbox' type='text' placeholder='测试的占位标题' />
+            <input ref='title' className={styles.textbox} type='text' placeholder='测试的占位标题' />
             </dd>
           </dl>
           <dl className={styles.clearfix}>
             <dt>推荐内容</dt>
             <dd>
-            <textarea ref='tweet-content' cols='30' rows='10'>textarea内容应该用value属性获取</textarea>
+              <textarea
+                ref='tweet-content'
+                className={styles['tweet-content']}
+                cols='30'
+                rows='10'
+                placeholder='textarea内容应该用value属性获取'
+              >
+              </textarea>
             </dd>
           </dl>
           <dl className={styles.clearfix}>
@@ -77,9 +90,9 @@ export default class Post extends Component {
             </dd>
           </dl>
           <dl className={styles.clearfix}>
-            <dt></dt>
+            <dt>&nbsp;</dt>
             <dd>
-            <button ref='submit' className={styles.button}>发送</button>
+              <button ref='submit' className={styles.button}>发送</button>
             </dd>
           </dl>
         </div>
